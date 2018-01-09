54e02a295f86c3912d81be736abfea45bfa7fb13
Victor Quiroz <victorhqc@gmail.com>
2018-01-09 00:46:33 -0600

    Implement redis cache Unfortunately the existing cache implemented by wal-mart labs is for react 15 only. For now Redis cache will do.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
+import redis from 'redis';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
 import server from './server';
@@ -11,14 +12,33 @@ import {
 const app = express();
 const PORT = process.env.port || 3000;
 
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(REDIS_PORT);
+
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+const cache = (req, res, next) => {
+  client.get('app', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    if (!data) {
+      next();
+      return;
+    }
+
+    res.send(data);
+  });
+};
+
+app.get('*', cache, (req, res) => {
   const promises = matchRoutes(Routes, req.path);
 
   Promise.all(promises).then(() => {
     const context = new ContextManager();
     const content = server({ req, context });
+    client.setex('app', 3600 * 24, content);
 
     if (context.get(NOT_FOUND)) {
       res.status(404);
